58b0ae2e0aedc95b099273f424fbdf3c92767b4e
ramosmejiamartin <ramosmejiamartin@f9aef56b-1955-0410-9fae-0947cacc7125>
2009-03-02 17:41:32 +0000

    General Refactor or the application. Inclusion of runner.

--- trunk/classes/controller/tisean_controller.py
+++ trunk/classes/controller/tisean_controller.py
@@ -5,11 +5,31 @@
 #  This package contains all the controller actions to be called from the view layer
 #
 
-from tisean_widgets import TiseanCommandForm
+import sys
+
+try:
+ 	import pygtk
+  	pygtk.require("2.0")
+except:
+  	pass
+try:
+	import gtk
+  	import gtk.glade
+except:
+	sys.exit(1)
+
+
+from tisean_widgets import TiseanCommandMenu
+from tisean_widgets import TiseanView
+from tisean_factory import TiseanGuiFactory
+from tisean_config import TiseanConfig
+from tisean_config import TiseanCommandConfig
+from tisean_runner import TiseanRunner
 
 ##
 # Development Documentation for the TiseanController class.
 # Represents the main controller of the application that connects the view layer with the model layer.
+# The controller is implemented as a mediator between view and model layer.
 # @author Martin Ramos Mejia
 # @version 0.1
 #
@@ -22,10 +42,26 @@ class TiseanController:
 	# @param mainInterface the TiseanGuiLauncher instance that controls the view.
 	# @param config the TiseanConfig instance that holds the current Tisean configuration
 	#
-	def __init__(self,mainInterface,config):
-		self.mainInterface = mainInterface
-		self.config = config
-	
+	def __init__(self):
+
+
+		#building of the configuration
+		self.config = TiseanConfig()
+		#building of the view
+		self.view = TiseanView(self)
+		
+		#building of command menu option widgets
+		tiseanCommandMenu = TiseanCommandMenu(self,self.config)
+		self.view.command_menu_setup(tiseanCommandMenu)
+				
+		#display of the main elements of the view on screen
+		self.view.show()
+
+		#we build the command runner
+		self.runner = TiseanRunner()
+		#we add the observer
+		self.runner.register_observer(self.view.get_console_updater())
+
 
 	##
 	# Callback that loads the gui for a certain command on the current view
@@ -33,6 +69,46 @@ class TiseanController:
 	# @param self the instance pointer
 	# @param menuitem the gui element that made the request
 	#
-	def load_command_form(self,menuitem):
-		form = TiseanCommandForm()
-		self.mainInterface.set_command_form(form)
+	def load_command_form(self,menuItem):
+		
+		#we obtain the command configuration
+		commandConfig = self.config.get_command_config(menuItem.get_command_name())
+		
+		factory = TiseanGuiFactory()
+		#we build the form
+		form = factory.create_form(commandConfig,self)
+
+		#we set it to the view
+		self.view.set_command_form(form)
+		form.show()
+	
+	##
+	#
+	#
+	#
+	#
+	def execute_command(self,button):
+	
+		#TODO form validation
+		form = self.view.get_command_form()
+		if (not form.validate()):
+			return false
+		
+		commandString = form.get_command_name()
+		
+		widgets = form.get_widgets()
+		
+		for widget in widgets:
+			if (widget.get_selected_value() is not ''):
+				commandString = commandString + ' ' + widget.get_parameter_name() + ' ' + widget.get_selected_value()
+
+		self.runner.execute_command(commandString)
+		
+	
+	##
+	# Closing of application Action
+	#
+	#
+	def application_close(self,window):
+
+		gtk.main_quit()
--- trunk/tiseangui.py
+++ trunk/tiseangui.py
@@ -3,6 +3,7 @@
 import sys
 
 sys.path.append('classes/gui/')
+sys.path.append('classes/runner/')
 sys.path.append('classes/controller/')
 sys.path.append('classes/config/')
 sys.path.append('config/')
@@ -18,9 +19,7 @@ try:
 except:
 	sys.exit(1)
 	
-from tisean_widgets import TiseanCommandMenu
 from tisean_controller import TiseanController
-from tisean_config import TiseanConfig
 
 ## @package tisean_launcher
 #  Development Documentation for the tisean_launcher package.
@@ -41,40 +40,8 @@ class TiseanGuiLauncher:
 	# @param self the instance pointer
 	#
 	def __init__(self):
-		
-		#Set the Glade file
-		self.gladefile = "classes/gui/glade/tiseangui.glade"
-		  
-		self.currentForm = None
-		self.mainInterface = gtk.glade.XML(self.gladefile)
-		self.mainWindow = self.mainInterface.get_widget("TiseanGUI")
-		self.consoleWindow = self.mainInterface.get_widget("TiseanGuiConsole")
-		self.menuBar = self.mainInterface.get_widget("TiseanMenuBar")
-		self.optionsCommandsHolder = self.mainInterface.get_widget("TiseanCommandOptionsHolder")
-		self.config = TiseanConfig()		
-		self.controller = TiseanController(self,self.config)
-		
-		self.mainWindow.show()
-		self.consoleWindow.show()
-		
-		if (self.mainWindow):
-			self.mainWindow.connect("destroy",gtk.main_quit)
+		self.controller = TiseanController()
 
-		#armado de opciones de menu de comandos
-		menuHolderItem = TiseanCommandMenu(self.controller,self.config)
-		self.menuBar.append(menuHolderItem)
-
-	##
-	# Sets the configuration form of a command on the main gui.
-	#
-	# @param self the instance pointer
-	# @param form TiseanCommandForm instance
-	#		
-	def set_command_form(self,form):
-		if (self.currentForm):
-			self.optionsCommandsHolder.remove(self.currentForm)
-		self.optionsCommandsHolder.pack_end(form)
-		self.currentForm = form
 
 if __name__ == "__main__":
 	tiseanGui = TiseanGuiLauncher()
